0fb3ab47558297df20d975ec82f6035927a42415
Elysiayn <wendy.lemus.wl@gmail.com>
2020-12-15 16:00:04 -0500

    modularized generatePage to create HTML template

--- app.js
+++ app.js
@@ -1,43 +1,10 @@
 //to use fs module
 const fs = require('fs');
+//to import page-template into current module
+const generatePage = require('./src/page-template.js');
 
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+const profileDataArgs = process.argv.slice(2);
 const [name, github] = profileDataArgs;
-// const name = profileDataArgs[0];
-// const github = profileDataArgs[1];  This does the same as above (assignment destructuring)
-
-// const printProfileData = profileDataArr => {
-//     // This...
-//     for (let i = 0; i < profileDataArr.length; i += 1) {
-//         console.log(profileDataArr[i]);
-//     }
-
-//     console.log('================');
-
-//     // Is the same as this...
-//     profileDataArr.forEach(profileItem => console.log(profileItem));
-// };
-
-// printProfileData(profileDataArgs);
-
-const generatePage = (userName, githubName) => {
-    return `
-    <!DOCTYPE html> 
-    <html lang="en"> 
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>Portfolio Demo</title>
-    </head>
-
-    <body>
-        <h1>${name}</h1>
-        <h2><a href="https://github.com/${github}">Github</a></h2>
-    </body>
-    </html>
-    `;
-};
 
 // Function to generate html using fs & including parameters
 fs.writeFile('./index.html', generatePage(name, github), err => {
